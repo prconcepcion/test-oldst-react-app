6debfd0775fde288c289af225e39ecb02a8b97c8
Pio Concepcion <prconcepcion@up.edu.ph>
2021-09-16 20:22:52 +0800

    Date can now display x days ago

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
       <header>
           <h1>Products Grid</h1>
 
-          <p>But first, a word from our sponsors:</p>
+          {/* <p>But first, a word from our sponsors:</p>
           <img
             className='ad'
             src={`http://localhost:8000/ads/?r=${Math.floor(Math.random()*1000)}`}
-            alt='ad'/>
+            alt='ad'/> */}
       </header>
       <Products />
     </div>
--- src/DisplayProducts.js
+++ src/DisplayProducts.js
@@ -1,10 +1,25 @@
+import { useState } from "react/cjs/react.development";
+
 const DisplayProducts = ({products}) => {
+
+    const dateFormat = (date) => {
+        const productDate = new Date(date);
+        const dateToday = new Date();
+        const differenceTime = dateToday.getTime() - productDate.getTime();
+        const minutes = 24 * 60 * 60 * 1000
+        return Math.round(differenceTime / minutes)
+    }
+
     console.log(products)
     return (
         <div className="product-list">
             {products.map((product) => (
                 <div className="product-details" key={ product.id }>
-                    <h4>{product.face}</h4>
+                    <div style={{ fontSize: product.size + 'px' }}>{ product.face }</div>
+                    <h5>Size: { product.size }px</h5>
+                    <h5>Price: ${ product.price / 100 }</h5>
+                    { (dateFormat(product.date)) > 6 && <h5>{ product.date }</h5> } 
+                    { (dateFormat(product.date)) <= 6 && <h5>{(dateFormat(product.date))} days ago</h5> } 
                 </div>
             ))}
         </div>
